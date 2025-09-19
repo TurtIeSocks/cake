6d1e50439642b5017f51f8c27aa9634dc88b1ca7
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2025-09-19 11:05:21 -0400

    switch to bun

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 import './services/logger.js'
 import { generate } from './modules/generate.js'
 import { update } from './modules/update.js'
-import { mailer } from './modules/mailer.js'
-import { loadApiCallHistory, saveApiCallHistory } from './services/cf.js'
+import { disableMailer, mailer } from './modules/mailer.js'
+import {
+  loadApiCallHistory,
+  saveApiCallHistory,
+} from './services/cf.js'
 
 const command = process.argv[2]
 
@@ -16,6 +19,9 @@ switch (command) {
     break
   case 'mailer':
     await mailer()
+    break
+  case 'disable-mailer':
+    await disableMailer()
     break
   default:
     await generate()
--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,6 +1,10 @@
 // @ts-check
-import { mapDomains, resetEmailRouting } from '../services/cf.js'
+import { disableEmailRouting, mapDomains, resetEmailRouting } from '../services/cf.js'
 
 export async function mailer() {
   await mapDomains(resetEmailRouting)
+}
+
+export async function disableMailer() {
+  await mapDomains(disableEmailRouting)
 }
